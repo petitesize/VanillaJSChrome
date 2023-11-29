c939f5b50302b4764acaa4489d89181e9f7ed498
jeeyng <petitesize22@gmail.com>
2023-11-29 14:06:41 +0900

    4-1. Login (Form Submission)

--- Readme.md
+++ Readme.md
@@ -3,3 +3,17 @@
 head에 다음을 작성
 
 <link rel="stylesheet" href="파일이름.css" />
+
+### 2. 로그인
+
+input의 유효성 검사를 작동시키기 위해서 input은 form 안에 있어야한다.
+input의 유효성 검사
+
+- required
+- maxlength=""
+
+### 3. form의 submit
+
+input 안의 button을 클릭하거나, type이 submit인 input을 클릭하면 작성한 form이 submit
+: 따라서 click에 집중할 필요가 없다!
+: 대신 submit할 때 웹사이트가 새로고침 된다. -> 내가 바라는게 아님!
--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log("hello", loginInput.value);
+  const username = loginInput.value; //로그인 input창의 값
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
