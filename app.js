3d94f984ca32a3fa4e455077b590b25167538c8c
petitesize <petitesize22@gmail.com>
2023-12-04 14:16:45 +0900

    4-6. Loading Username

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const greeting = document.querySelector("#greeting");
 
 //string만 포함된 변수는 대문자로
 const HIDDEN_CLASSNAME = "hidden";
-
-const link = document.querySelector("a");
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME); //loginForm에 hidden 클래스 추가
-  greeting.innerText = `Hello ${username}`;
-  localStorage.setItem("username", username);
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
 function handleLinkClick(event) {
@@ -21,5 +19,19 @@ function handleLinkClick(event) {
   console.dir(event);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //유저 정보가 없으면, login form의 hidden class를 없애주어 form을 보여준다
+  //유저 정보를 입력해야 하기 때문에
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // greetings를 보여준다
+  paintGreetings(savedUsername);
+}
