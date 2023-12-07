07029dd40612363df6fa456e6982ff69cb5d444c
petitesize <petitesize22@gmail.com>
2023-12-07 14:03:38 +0900

    6-1. Background
    html에 img와 src 추가하는 법

---
+++ js/backgrounds.js
@@ -0,0 +1,9 @@
+const images = ["01.astronomy.jpg", "02.rose-petals.jpg", "03.snowfall.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
